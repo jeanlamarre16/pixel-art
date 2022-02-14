072a8821bb0135c7f91bf533ca388be2e8c31172
unknown <jeanlamarre16@yahoo.fr>
2022-02-14 18:11:35 +0100

    final Version of pixel art

--- pixelArt.js
+++ pixelArt.js
@@ -1,55 +1,62 @@
 
-const bouton = document.getElementById('generer-grille');
-const palette = document.getElementById("palette");
+const bouton = document.getElementById('grid-generator');
+const palette = document.getElementById("div-palette");
 let divTableau = document.querySelector('#div-tableau');
-const table = document.createElement('table');
 
 let  inputLignes = document.querySelector('.nbLignes');
 let  inputColonnes = document.querySelector('.nbColonnes');
 
 bouton.addEventListener('click', function () {
-	let genererdivTableau = "Générer";
-	if(bouton.textContent === genererdivTableau) {
-		createTable();
-		bouton.textContent = 'Reset';
-		inputLignes.setAttribute('disabled', '');
-		inputColonnes.setAttribute('disabled', '');
-		palette.style.visibility="visible";
+	let genererdivTableau = "Générer Tableau";
+	if(controlerSaisie()) {
+		if(bouton.textContent === genererdivTableau) {
+			createTable();
+			bouton.textContent = 'Reset';
+			inputLignes.setAttribute('disabled', '');
+			inputColonnes.setAttribute('disabled', '');
+			palette.style.visibility="visible";
+		} else  {
+			resetTable();
+			bouton.textContent=genererdivTableau;
+			inputLignes.removeAttribute("disabled", "");
+			inputColonnes.removeAttribute("disabled", "");
+			palette.style.visibility="hidden";
+		}
 	} else  {
-		resetTable();
-		bouton.textContent=genererdivTableau;
-		inputLignes.removeAttribute("disabled", "");
-		inputColonnes.removeAttribute("disabled", "");
-		palette.style.visibility="hidden";
-	}	
+		controlerSaisie();
+	}
 })
 
-function createTable() {
+function controlerSaisie() {
 	if (inputLignes.value < 2 || inputLignes.value > 20 || inputLignes === " " ) {
-		inputColonnes.value = " ";
-		inputColonnes.focus();
+		inputLignes.value = " ";
+		inputLignes.focus();
+		return false;
 	} else if (inputColonnes.value < 2 || inputColonnes.value > 20  || inputColonnes === " ") {
 		inputColonnes.value = " ";
 		inputColonnes.focus();
-	} else {
-		for (let i = 0; i < inputLignes.value; i++) {
-			let rows = document.createElement("tr");
-			table.appendChild(rows);
-		
-			for (let j = 0; j < inputColonnes.value; j++) {
-				let columns = document.createElement("td");
-				columns.setAttribute('onclick', 'setColor(this)');
-				rows.appendChild(columns);
-			}
-		}
-		divTableau.appendChild(table);
+		return false;
 	}
+	return true;
+}
+
+function createTable() {
+	const table = document.createElement('table');	
+	for (let i = 0; i < inputLignes.value; i++) {
+		let rows = document.createElement("tr");
+		table.appendChild(rows);
+	
+		for (let j = 0; j < inputColonnes.value; j++) {
+			let columns = document.createElement("td");
+			columns.setAttribute('onclick', 'setColor(this)');
+			rows.appendChild(columns);
+		}
+	}
+	divTableau.appendChild(table);
 }
 
 function resetTable() {
-	inputColonnes.value = "";
-	inputLignes.value= "";
-	divTableau.removeChild(table);
+	divTableau.removeChild(document.querySelector('div#div-tableau table'));
 }
 
 let brushColor = "";
