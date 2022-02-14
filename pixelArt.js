053c247f92e2611ca3cd01a672c995ac8077a041
unknown <jeanlamarre16@yahoo.fr>
2022-02-14 15:33:36 +0100

    correction

--- pixelArt.js
+++ pixelArt.js
@@ -1,39 +1,40 @@
 
 const bouton = document.getElementById('generer-grille');
 const palette = document.getElementById("palette");
-let grille = document.querySelector('#grille-tableau');
+let divTableau = document.querySelector('#div-tableau');
+const table = document.createElement('table');
 
 let  inputLignes = document.querySelector('.nbLignes');
 let  inputColonnes = document.querySelector('.nbColonnes');
 
 bouton.addEventListener('click', function () {
-	let genererGrille = "Générer";
-	if(bouton.textContent === genererGrille) {
+	let genererdivTableau = "Générer";
+	if(bouton.textContent === genererdivTableau) {
 		createTable();
 		bouton.textContent = 'Reset';
 		inputLignes.setAttribute('disabled', '');
-		inputColonnes.removeAttribute('disabled', '');
+		inputColonnes.setAttribute('disabled', '');
 		palette.style.visibility="visible";
 	} else  {
 		resetTable();
-		bouton.textContent=genererGrille;
-		inputLignes.setAttribute("disabled", "false");
-		inputColonnes.setAttribute("disabled", "false");
+		bouton.textContent=genererdivTableau;
+		inputLignes.removeAttribute("disabled", "");
+		inputColonnes.removeAttribute("disabled", "");
 		palette.style.visibility="hidden";
 	}	
 })
 
 function createTable() {
 	if (inputLignes.value < 2 || inputLignes.value > 20 || inputLignes === " " ) {
-		nbLignes.value = " ";
-		nbLignes.focus();
+		inputColonnes.value = " ";
+		inputColonnes.focus();
 	} else if (inputColonnes.value < 2 || inputColonnes.value > 20  || inputColonnes === " ") {
 		inputColonnes.value = " ";
 		inputColonnes.focus();
 	} else {
 		for (let i = 0; i < inputLignes.value; i++) {
 			let rows = document.createElement("tr");
-			grille.appendChild(rows);
+			table.appendChild(rows);
 		
 			for (let j = 0; j < inputColonnes.value; j++) {
 				let columns = document.createElement("td");
@@ -41,14 +42,14 @@ function createTable() {
 				rows.appendChild(columns);
 			}
 		}
+		divTableau.appendChild(table);
 	}
 }
 
 function resetTable() {
-	let elt = document.querySelectorAll("tr");
-	elt.forEach(element => {
-		grille.removeChild(element);
-	});
+	inputColonnes.value = "";
+	inputLignes.value= "";
+	divTableau.removeChild(table);
 }
 
 let brushColor = "";
